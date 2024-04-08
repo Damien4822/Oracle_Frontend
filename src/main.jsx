09568eedb62e4ce930ec94aef446f0dfe3dfcebf
Damien4822 <114857862+Damien4822@users.noreply.github.com>
2024-04-09 03:38:07 +0700

    configurating files(ongoing)

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import 'react-router-dom'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter> 
   </React.StrictMode>,
 )
